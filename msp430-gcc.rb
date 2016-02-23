173fd75ad0b718d9fe7481610aa62679a4de5834
Sam Petersen <sam.petersen@ll.mit.edu>
2016-02-23 14:09:05 -0500

    Updated mcu and gcc

--- msp430-gcc.rb
+++ msp430-gcc.rb
@@ -23,6 +23,16 @@ class Msp430Gcc < Formula
     sha1 "3e70230f6052ed30d1a288724f2b97ab47581489"
   end
 
+  patch do
+    url "http://sourceforge.net/p/mspgcc/bugs/352/attachment/0001-SF-352-Bad-code-generated-pushing-a20-from-stack.patch"
+    sha1 "c1f17649634995399ad45c30b0c65cf18320b784"
+  end
+  
+  patch do
+    url "http://sourceforge.net/p/mspgcc/bugs/_discuss/thread/fd929b9e/db43/attachment/0001-SF-357-Shift-operations-may-produce-incorrect-result.patch"
+    sha1 "76c464df8e76f98444edb457db45a68a71ddc83e"
+  end
+
   def install
     # The bootstrap process uses "xgcc", which doesn't have these flags. This
     # results in an error like the following:
